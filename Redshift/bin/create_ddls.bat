c43d57d8f02653b376d51ac18b0d0c54f85c09a4
Jose Chacón Bogarín <jchacon@mobilize.net>
2022-03-10 14:14:14 -0600

    Simplified procedure extraction

--- Redshift/bin/create_ddls.bat
+++ Redshift/bin/create_ddls.bat
@@ -5,17 +5,15 @@ REM                       below to match your environment. Once completed, your 
 REM ---- Variables to change ----
 
 REM General Variables
-SET OUTPUT_PATH="<C:\example\path>"
+SET OUTPUT_PATH="C:\_rs_out"
 
 REM AWS RedShift Variables
-SET RS_CLUSTER="<redshift_cluster_identifier>"
-SET RS_DATABASE="<redshift_database>"
-SET RS_SECRET_ARN="<secret_arn>"
+SET RS_CLUSTER="redshift-cluster-1"
+SET RS_DATABASE="dev"
+SET RS_SECRET_ARN="arn:aws:secretsmanager:us-east-2:049502660834:secret:dev/redsfhift-cluster-1-edSdeq"
 
 REM Script Variables
 SET SCHEMA_FILTER="lower(schemaname) LIKE '%%'"
-SET BATCH_WAIT="0.2"
-SET THREADS="4"
 REM ---- END: Variables to change ----
 
 mkdir %OUTPUT_PATH%
@@ -26,4 +24,4 @@ mkdir %OUTPUT_PATH%\object_extracts\DDL
 REM mkdir %OUTPUT_PATH%\object_extracts\Reports
 REM mkdir %OUTPUT_PATH%\object_extracts\Storage
 
-python ../scripts/_ddl_extractor.py --rs-cluster %RS_CLUSTER% --rs-database %RS_DATABASE% --rs-secret-arn %RS_SECRET_ARN% --output-path %OUTPUT_PATH% --schema-filter %SCHEMA_FILTER% --batch-wait %BATCH_WAIT% --threads %THREADS%
+python ../scripts/_ddl_extractor.py --rs-cluster %RS_CLUSTER% --rs-database %RS_DATABASE% --rs-secret-arn %RS_SECRET_ARN% --output-path %OUTPUT_PATH% --schema-filter %SCHEMA_FILTER%
